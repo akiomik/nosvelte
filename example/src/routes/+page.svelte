9248fd1a1388b916857e6b37536b14d5a32a10d6
Akiomi Kamakura <akiomik@gmail.com>
2023-06-04 20:31:10 +0900

    Remove example

--- example/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import { NostrApp, Metadata } from 'svelte-nostr';
-</script>
-
-<NostrApp relays={['wss://relay.damus.io']}>
-  <Metadata pubkey="4d39c23b3b03bf99494df5f3a149c7908ae1bc7416807fdd6b34a31886eaae25" />
-</NostrApp>
