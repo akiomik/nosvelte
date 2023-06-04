514e053ee302ec87d3c2431009122c3fd532077a
Akiomi Kamakura <akiomik@gmail.com>
2023-06-04 21:08:39 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,15 @@
 </script>
 
 <NostrApp relays={['wss://relay.damus.io']}>
-  <Metadata pubkey="4d39c23b3b03bf99494df5f3a149c7908ae1bc7416807fdd6b34a31886eaae25" />
+  <Metadata pubkey="4d39c23b3b03bf99494df5f3a149c7908ae1bc7416807fdd6b34a31886eaae25" let:data>
+    <div slot="loading">
+      <p>Loading...</p>
+    </div>
+
+    <div slot="error" let:error>
+      <p>{error}</p>
+    </div>
+
+    <p>{JSON.parse(data.event.content).name}</p>
+  </Metadata>
 </NostrApp>
