7380bd2f827c8054fad8d6f44219c8ad906fed04
Akiomi Kamakura <akiomik@gmail.com>
2023-06-05 02:31:57 +0900

    Improve example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 
   let relays: string[] = ['wss://relay.damus.io'];
   let newRelay = '';
+  let pubkey = '4d39c23b3b03bf99494df5f3a149c7908ae1bc7416807fdd6b34a31886eaae25';
 
   const unique = <A>(xs: A[]) => [...new Set(xs)];
 
@@ -22,30 +23,54 @@
 <h1>Nostr profile viwer</h1>
 
 <NostrApp {relays}>
-  <ul>
-    {#each relays as relay}
-      <li>
-        {relay}
-        <button on:click={() => removeRelay(relay)}>Remove</button>
-      </li>
-    {/each}
-  </ul>
+  <section>
+    <h2>Relays</h2>
 
-  <label>
-    New Relay
-    <input type="text" bind:value={newRelay} />
-  </label>
-  <button on:click={addRelay}>Add</button>
+    <ul>
+      {#each relays as relay}
+        <li>
+          {relay}
+          <button on:click={() => removeRelay(relay)}>Remove</button>
+        </li>
+      {/each}
+    </ul>
 
-  <Metadata pubkey="4d39c23b3b03bf99494df5f3a149c7908ae1bc7416807fdd6b34a31886eaae25" let:metadata>
-    <div slot="loading">
-      <p>Loading...</p>
-    </div>
+    <form on:submit={addRelay}>
+      <input
+        type="text"
+        bind:value={newRelay}
+        placeholder="wss://..."
+        pattern="wss?://.+"
+        required
+      />
+      <button type="submit">Add</button>
+    </form>
+  </section>
 
-    <div slot="error" let:error>
-      <p>{error}</p>
-    </div>
+  <section>
+    <h2>Profile</h2>
 
-    <p>{JSON.parse(metadata.content).name}</p>
-  </Metadata>
+    <input
+      type="text"
+      bind:value={pubkey}
+      placeholder="pubkey"
+      pattern="[a-z0-9]{64}"
+      required
+      size="64"
+    />
+
+    {#if pubkey}
+      <Metadata {pubkey} let:metadata>
+        <div slot="loading">
+          <p>Loading...</p>
+        </div>
+
+        <div slot="error" let:error>
+          <p>{error}</p>
+        </div>
+
+        <p>{JSON.parse(metadata.content).name}</p>
+      </Metadata>
+    {/if}
+  </section>
 </NostrApp>
