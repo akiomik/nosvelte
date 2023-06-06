242a0f64c0e2512b42e438f456fb531665ca2611
Akiomi Kamakura <akiomik@gmail.com>
2023-06-06 12:08:25 +0900

    Add simple example

---
+++ src/routes/dynamic-data-loading/+page.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+  /**
+   * @license Apache-2.0
+   * @copyright 2023 Akiomi Kamakura
+   */
+
+  import Metadata from '$lib/Metadata.svelte';
+  import NostrApp from '$lib/NostrApp.svelte';
+
+  let relays: string[] = ['wss://relay.damus.io', 'wss://relay.snort.social'];
+  let newRelay = '';
+  let pubkey = '4d39c23b3b03bf99494df5f3a149c7908ae1bc7416807fdd6b34a31886eaae25';
+
+  const unique = <A>(xs: A[]) => [...new Set(xs)];
+
+  const removeRelay = (relay: string) => {
+    relays = relays.filter((r) => r !== relay);
+  };
+
+  const addRelay = () => {
+    if (newRelay) {
+      relays = unique([...relays, newRelay]);
+      newRelay = '';
+    }
+  };
+</script>
+
+<NostrApp {relays} let:connections>
+  <h1>dynamic-data-loading</h1>
+
+  <section>
+    <h2>Relays</h2>
+
+    <ul>
+      {#each connections as connection (connection.from)}
+        <li>
+          {connection.from}
+          /
+          {connection.state}
+          <button on:click={() => removeRelay(connection.from)}>Remove</button>
+        </li>
+      {/each}
+    </ul>
+
+    <form on:submit={addRelay}>
+      <input
+        type="text"
+        bind:value={newRelay}
+        placeholder="wss://..."
+        pattern="wss?://.+"
+        required
+      />
+      <button type="submit">Add</button>
+    </form>
+  </section>
+
+  <section>
+    <h2>Profile</h2>
+
+    <input
+      type="text"
+      bind:value={pubkey}
+      placeholder="pubkey"
+      pattern="[a-z0-9]{64}"
+      required
+      size="64"
+    />
+
+    {#if pubkey}
+      <Metadata {pubkey} let:metadata>
+        <div slot="loading">
+          <p>Loading...</p>
+        </div>
+
+        <div slot="error" let:error>
+          <p>{error}</p>
+        </div>
+
+        {#if metadata}
+          <p>{JSON.parse(metadata.content).name}</p>
+        {:else}
+          <p>Not found</p>
+        {/if}
+      </Metadata>
+    {/if}
+  </section>
+</NostrApp>
+
+<a href="/">Back to top</a>
---
+++ src/routes/simple/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  /**
+   * @license Apache-2.0
+   * @copyright 2023 Akiomi Kamakura
+   */
+
+  import Metadata from '$lib/Metadata.svelte';
+  import NostrApp from '$lib/NostrApp.svelte';
+
+  const relays: string[] = ['wss://relay.damus.io', 'wss://relay.snort.social'];
+  const pubkey = '4d39c23b3b03bf99494df5f3a149c7908ae1bc7416807fdd6b34a31886eaae25';
+</script>
+
+<NostrApp {relays}>
+  <h1>simple</h1>
+
+  <section>
+    <Metadata {pubkey} let:metadata>
+      <div slot="loading">
+        <p>Loading...</p>
+      </div>
+
+      <div slot="error" let:error>
+        <p>{error}</p>
+      </div>
+
+      {#if metadata}
+        <p>{JSON.parse(metadata.content).name}</p>
+      {:else}
+        <p>Not found</p>
+      {/if}
+    </Metadata>
+  </section>
+</NostrApp>
+
+<a href="/">Back to top</a>
