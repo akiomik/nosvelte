3ad5538fa554d471a6bf10dc12595820d9fa4903
Akiomi Kamakura <akiomik@gmail.com>
2023-06-06 15:10:16 +0900

    Add Text component

---
+++ src/lib/Text.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  /**
+   * @license Apache-2.0
+   * @copyright 2023 Akiomi Kamakura
+   */
+
+  import type { Nostr } from 'rx-nostr';
+
+  import type { RxReqBase } from './store.js';
+  import { app, useText } from './store.js';
+
+  export let id: string;
+  export let req: RxReqBase | undefined = undefined;
+
+  // TODO: Check if $app.client is defined
+  $: result = useText($app.client, id, req);
+  $: data = result.data;
+  $: isLoading = result.isLoading;
+  $: error = result.error;
+  $: isSuccess = result.isSuccess;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface $$Slots {
+    default: { text: Nostr.Event; loading: boolean; success: boolean };
+    loading: Record<never, never>;
+    error: { error: Error };
+  }
+</script>
+
+{#if $isLoading && $data === undefined}
+  <slot name="loading" />
+{:else if $error}
+  <slot name="error" error={$error} />
+{:else}
+  <slot text={$data?.event} loading={$isLoading} success={$isSuccess} />
+{/if}
