373a77755833cabe0d0b26e1088ca0a12b41d967
Akiomi Kamakura <akiomik@gmail.com>
2023-06-13 20:39:29 +0900

    Update example code in README.md

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,7 @@
 <nav>
   <ul>
     <li><a href="/simple">simple</a></li>
+    <li><a href="/readme">readme</a></li>
     <li><a href="/reactive-data-loading">reactive-data-loading</a></li>
     <li><a href="/n-plus-1">n-plus-1 (TBU)</a></li>
     <li><a href="/timeline">timeline</a></li>
--- src/routes/readme/+page.svelte
+++ src/routes/readme/+page.svelte
@@ -1,17 +1,15 @@
-# nosvelte
-
-An experimental Svelte library for building [Nostr](https://nostr.com) apps easily.
-Highly inspired by sveltefire.
-
-```svelte
 <script lang="ts">
-  import { Metadata, NostrApp } from 'nosvelte';
+  import Metadata from '$lib/components/Metadata.svelte';
+  import NostrApp from '$lib/components/NostrApp.svelte';
+  import Text from '$lib/components/Text.svelte';
 
   const relays = ['wss://relay.damus.io', 'wss://relay.snort.social'];
   const id = 'b6cf76789bd25d11eafa65d28c16dd640056919f703191aa06619c5b21f732e3';
 </script>
 
 <NostrApp {relays}>
+  <h1>readme</h1>
+
   <Text queryKey={[id]} {id} let:text>
     <Metadata queryKey={['metadata', text.pubkey]} pubkey={text.pubkey} let:metadata>
       <!-- Shows "jack: Japan confirmed punk" -->
@@ -19,14 +17,3 @@ Highly inspired by sveltefire.
     </Metadata>
   </Text>
 </NostrApp>
-```
-
-## Features
-
-- Zero-configuration
-- Svelte stores and components to use Nostr events
-- Caching requests
-
-## Exapmles
-
-You can see some example codes in [src/routes](https://github.com/akiomik/nosvelte/tree/main/src/routes).
