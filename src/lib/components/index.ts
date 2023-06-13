008e36cf7a9eed9f52a06923a349e8f71bfdec46
Akiomi Kamakura <akiomik@gmail.com>
2023-06-14 04:52:20 +0900

    Add RelayListMetadata component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -9,6 +9,7 @@ export { default as Metadata } from './Metadata.svelte';
 export { default as Mute } from './Mute.svelte';
 export { default as NostrApp } from './NostrApp.svelte';
 export { default as Pin } from './Pin.svelte';
+export { default as RelayListMetadata } from './RelayListMetadata.svelte';
 export { default as Text } from './Text.svelte';
 export { default as UniqueEventList } from './UniqueEventList.svelte';
 export { default as UserReactionList } from './UserReactionList.svelte';
