d385b6f6d39dad8edb08c5c5014bb406c78f141a
Akiomi Kamakura <akiomik@gmail.com>
2023-06-14 04:51:35 +0900

    Add Pin component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -8,6 +8,7 @@ export { default as Contacts } from './Contacts.svelte';
 export { default as Metadata } from './Metadata.svelte';
 export { default as Mute } from './Mute.svelte';
 export { default as NostrApp } from './NostrApp.svelte';
+export { default as Pin } from './Pin.svelte';
 export { default as Text } from './Text.svelte';
 export { default as UniqueEventList } from './UniqueEventList.svelte';
 export { default as UserReactionList } from './UserReactionList.svelte';
