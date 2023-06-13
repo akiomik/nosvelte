e52bfe80776d50cd31892ce5a89022524613a3d6
Akiomi Kamakura <akiomik@gmail.com>
2023-06-14 04:09:27 +0900

    Add EventList component

---
+++ src/lib/components/EventList.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  /**
+   * @license Apache-2.0
+   * @copyright 2023 Akiomi Kamakura
+   */
+
+  import type { QueryKey } from '@tanstack/svelte-query';
+  import type { Nostr } from 'rx-nostr';
+
+  import type { ReqStatus, RxReqBase } from '$lib/stores/index.js';
+  import { app, useEventList } from '$lib/stores/index.js';
+
+  export let queryKey: QueryKey;
+  export let ids: string[];
+  export let req: RxReqBase | undefined = undefined;
+
+  // TODO: Check if $app.rxNostr is defined
+  $: result = useEventList($app.rxNostr, queryKey, ids, req);
+  $: data = result.data;
+  $: status = result.status;
+  $: error = result.error;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface $$Slots {
+    default: { events: Nostr.Event[]; status: ReqStatus };
+    loading: Record<never, never>;
+    error: { error: Error };
+    nodata: Record<never, never>;
+  }
+</script>
+
+{#if $error}
+  <slot name="error" error={$error} />
+{:else if $data?.length > 0}
+  <slot events={$data?.map(({ event }) => event)} status={$status} />
+{:else if $status === 'loading'}
+  <slot name="loading" />
+{:else}
+  <slot name="nodata" />
+{/if}
